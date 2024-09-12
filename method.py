3b4a258fd1e8a37889a4790181a242888fb75989
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-09-12 17:30:06 +0300

    Update method.py

--- method.py
+++ method.py
@@ -1,8 +1,17 @@
+#create class dog
 class Dog:
+    def __init__(self, name):
+        self.name = name
+         
     def bark(self):
         print("bark")
+ #you can create as many functions as you want in a class
+     def add_one(self, x):
+         return x + 1
 
+#create an instance 
 d  = Dog()
-d
+
 d.bark()
+print(d.add_one(5))
 print(type(d))
